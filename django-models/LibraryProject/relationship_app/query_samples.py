8d179f81c6892fd85b305eb5f0471b9fa0073852
derrick056 <derrickgatete056@gmail.com>
2025-02-23 16:25:54 +0200

    django models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ import os
 import django
 
 # Set up Django environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_models.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")  # Ensure the correct project name
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
@@ -19,22 +19,23 @@ def get_books_in_library(library_name):
     books = library.books.all()
     return books
 
-# Retrieve the librarian for a library
+# Retrieve the librarian for a library (Fix applied here)
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    librarian = Librarian.objects.get(library=library)  # Correct usage
+    return librarian
 
-# Sample Data Creation
+# Sample Data Creation (Only run this once to avoid duplicate records)
 if __name__ == "__main__":
     # Creating objects
-    author = Author.objects.create(name="J.K. Rowling")
-    book1 = Book.objects.create(title="Harry Potter and the Sorcerer's Stone", author=author)
-    book2 = Book.objects.create(title="Harry Potter and the Chamber of Secrets", author=author)
-    
-    library = Library.objects.create(name="Central Library")
+    author, _ = Author.objects.get_or_create(name="J.K. Rowling")
+    book1, _ = Book.objects.get_or_create(title="Harry Potter and the Sorcerer's Stone", author=author)
+    book2, _ = Book.objects.get_or_create(title="Harry Potter and the Chamber of Secrets", author=author)
+
+    library, _ = Library.objects.get_or_create(name="Central Library")
     library.books.add(book1, book2)
 
-    librarian = Librarian.objects.create(name="John Doe", library=library)
+    librarian, _ = Librarian.objects.get_or_create(name="John Doe", library=library)
 
     # Running Queries
     print("Books by J.K. Rowling:", list(get_books_by_author("J.K. Rowling")))
