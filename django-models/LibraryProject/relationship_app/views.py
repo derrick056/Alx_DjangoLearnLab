35b20724ab1ad5e70dea1bb02750f8cb2fc9637a
derrick056 <derrickgatete056@gmail.com>
2025-02-24 18:51:24 +0200

    this time

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import user_passes_test
 from .models import Book
 from django.contrib.auth.decorators import permission_required
 
+
 # Create your views here.
 from .models import Book
 from .models import Library
@@ -88,25 +89,10 @@ def member_view(request):
 def register(request):
     return render(request, "relationship_app/register.html")
 
-@permission_required('relationship_app.can_add_book', raise_exception=True)
+def is_admin(user):
+    return user.is_superuser  # Or any custom check
+
+@user_passes_test(is_admin, login_url="/login/")
 def add_book(request):
-    if request.method == "POST":
-        # Add book logic here
-        pass
+    # Add book logic here
     return render(request, "relationship_app/add_book.html")
-
-@permission_required('relationship_app.can_change_book', raise_exception=True)
-def edit_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    if request.method == "POST":
-        # Edit book logic here
-        pass
-    return render(request, "relationship_app/edit_book.html", {"book": book})
-
-@permission_required('relationship_app.can_delete_book', raise_exception=True)
-def delete_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    if request.method == "POST":
-        book.delete()
-        return redirect("book_list")
-    return render(request, "relationship_app/delete_book.html", {"book": book})
