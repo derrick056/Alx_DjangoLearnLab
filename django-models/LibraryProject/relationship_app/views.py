59b0651cda6ab0e69385cdda58d399db065355ee
derrick056 <derrickgatete056@gmail.com>
2025-02-24 18:49:44 +0200

    this time

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404,redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.forms import UserCreationForm
@@ -6,6 +6,8 @@ from django.views.generic.detail import DetailView
 from django.views import View
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import user_passes_test
+from .models import Book
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
 from .models import Book
@@ -85,3 +87,26 @@ def member_view(request):
 
 def register(request):
     return render(request, "relationship_app/register.html")
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == "POST":
+        # Add book logic here
+        pass
+    return render(request, "relationship_app/add_book.html")
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        # Edit book logic here
+        pass
+    return render(request, "relationship_app/edit_book.html", {"book": book})
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.delete()
+        return redirect("book_list")
+    return render(request, "relationship_app/delete_book.html", {"book": book})
