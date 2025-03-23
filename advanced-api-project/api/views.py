1203400f00b1a2b51f3df00065e2f4b24be37cb8
derrick056 <derrickgatete056@gmail.com>
2025-03-23 14:18:14 +0200

    filtering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 # ---- BOOK VIEWS ----
 
