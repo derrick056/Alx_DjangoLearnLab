67a788bcb3be4e4cfd93e36ed4c08aa76d06963f
derrick056 <derrickgatete056@gmail.com>
2025-03-30 15:55:46 +0200

    new

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
 
-# Create your views here.
+# ListView - Display all blog posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # Template for displaying posts
+    context_object_name = 'posts'
+    ordering = ['-published_date']  # Show latest posts first
+
+# DetailView - Show a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+# CreateView - Authenticated users can create a post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Set logged-in user as author
+        return super().form_valid(form)
+
+# UpdateView - Only post authors can edit their posts
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Allow only authors to edit
+
+# DeleteView - Only post authors can delete their posts
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Allow only authors to delete
