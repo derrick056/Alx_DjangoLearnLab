d1be1a475b8c6a204b286373e4d9fee32aabca13
derrick056 <derrickgatete056@gmail.com>
2025-03-30 17:19:04 +0200

    complicated

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Comment
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -15,3 +17,11 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Add a comment...'}),
+        }
